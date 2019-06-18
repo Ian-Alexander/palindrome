167b0fb2ebf6991881651a8d4c11ba972c0ac429
Ian Alexander and Mike McShane <ian@>
2019-06-17 20:36:15 -0700

    palindrome

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+// business logic
+// var palindrome = function(string) {
+//
+// }
+
+
+// user interface logic
+$(document).ready(function() {
+  $("form#palindrome").submit(function(event) {
+    event.preventDefault();
+    var string = $("input#string").val();
+
+    var array1 = Array.from(string);
+
+    var revArray = array1.reverse();
+
+    var revString = revArray.join('');
+
+    $(".phrase").text(string);
+
+    // if (string === revString) {
+    //   $(".not").text("");
+    // } else {
+    //   $(".not").text("not");
+    // }
+
+    if (string === revString) {
+      $("#result").text("That is a palindrome");
+    } else {
+      $("#result").text("That is not a palindrome");
+    }
+
+    $("#result").show;
+
+    console.log();
+    
+  });
+});
